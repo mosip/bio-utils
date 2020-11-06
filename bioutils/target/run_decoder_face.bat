4fce6a866d3f229f02723467ea46fdbfb2eebafe
JanardhanBS-SyncByte <janardhan@syncbyte.in>
2020-11-06 13:32:21 +0530

    Modified Bio-utils

--- bioutils/target/run_decoder_face.bat
+++
@@ -1,1 +0,0 @@
-java -cp bioutils-0.0.1-SNAPSHOT.jar;lib\* org.mosip.bio.utils.test.BioUtilApplication "org.mosip.bio.utils.convert.iso.to.jp2000=1" "mosip.mock.sbi.biometric.type.face.folder.path=/BiometricInfo/Face/" "org.mosip.bio.utils.biometric.type.file.iso=info.iso"  "mosip.mock.sbi.biometric.subtype.unknown=UNKNOWN"
