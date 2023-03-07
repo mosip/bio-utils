7f4dd246a1ea8651a7eabd3b649eeee69397aea5
ckm007 <chandrakeshavmishra@gmail.com>
2023-03-07 10:36:23 +0530

    [DSD-2180] moved kernel-biometrics-api to bio-utils

---
+++ kernel-biometrics-api/src/main/java/io/mosip/kernel/biometrics/constant/BiometricFunction.java
@@ -0,0 +1,13 @@
+package io.mosip.kernel.biometrics.constant;
+
+import java.io.Serializable;
+
+public enum BiometricFunction implements Serializable {
+	
+	QUALITY_CHECK,
+	SEGMENT,
+	EXTRACT,
+	CONVERT_FORMAT,
+	MATCH;
+
+}
---
+++ kernel-biometrics-api/src/main/java/io/mosip/kernel/biometrics/constant/Match.java
@@ -0,0 +1,11 @@
+package io.mosip.kernel.biometrics.constant;
+
+import java.io.Serializable;
+
+public enum Match implements Serializable {
+	
+	MATCHED,
+	NOT_MATCHED,
+	ERROR;	
+
+}
