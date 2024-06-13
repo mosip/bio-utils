ea8f0e34abf38b96ed79a4b2e03810773cb80faa
JanardhanBS-SyncByte <janardhan@syncbyte.in>
2024-06-13 16:07:17 +0530

    [MOSIP-33143] Merge to develop
    Signed-off-by: JanardhanBS-SyncByte <janardhan@syncbyte.in>

--- test/run_decoder_jp2000.bat
+++ test/run_decoder_jp2000.bat
@@ -1,1 +1,1 @@
-java -cp bioutils-1.2.1-java21-SNAPSHOT.jar;lib\* io.mosip.biometrics.util.test.Jp2000DecodeApplication "io.mosip.biometrics.util.image.type.jp2000=0" "mosip.mock.sbi.biometric.type.face.folder.path=/BiometricInfo/Face/" "mosip.mock.sbi.biometric.type.file.image=info1.jp2"
+java -cp bioutils-1.2.1-SNAPSHOT.jar;lib\* io.mosip.biometrics.util.test.Jp2000DecodeApplication "io.mosip.biometrics.util.image.type.jp2000=0" "mosip.mock.sbi.biometric.type.face.folder.path=/BiometricInfo/Face/" "mosip.mock.sbi.biometric.type.file.image=info1.jp2"
