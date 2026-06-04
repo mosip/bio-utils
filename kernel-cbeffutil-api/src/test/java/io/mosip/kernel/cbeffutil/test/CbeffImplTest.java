85596913865bee09421935a7062e9f96c124d629
kameshsr <47484458+kameshsr@users.noreply.github.com>
2026-06-04 10:40:57 +0530

    #1830 https://github.com/mosip/commons/issues/1830 Move release-1.3.x changes to develop (#257)
    * chore: merge upstream mosip/bio-utils v1.3.1 changes
    
    - Updates all module versions to 1.3.1
    - Adds ObjectFactory to kernel-biometrics-api entities
    - Updates CbeffValidator, ByteArrayToIntArraySerializer, IntArrayToByteArrayDeserializer
    - Adds THIRD-PARTY-NOTICES.txt to all modules
    - Updates CBEFF XSD schema and NOTICE files
    - Updates biosdk-provider and cbeffutil-api pom dependencies
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * fix: resolve ambiguous validateXML method reference in CbeffImplTest
    
    Use typed any(byte[].class) matchers to disambiguate between
    validateXML(byte[], byte[]) and validateXML(Schema, byte[]) overloads.
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * fix: use nullable matcher for null xsd in validateXML mock
    
    CbeffImpl.validateXML(byte[]) passes a null xsd field to the two-arg
    overload, which then calls CbeffXSDValidator.validateXML(null, xmlBytes).
    Mockito's any(byte[].class) does not match null, causing the mock to
    not fire and returning false. Use nullable(byte[].class) to correctly
    match the null first argument.
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * correct snapshot version to 1.4.0-Snapshoshot
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * #1830 added Agenets.md
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * #1830 corrected snapshot version
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * #1830 Removed duplicate dependency
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * #1830 Correct dependency to 1.4.0-SNAPSHOT
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * #1830 Corrected license
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * #1830 Corrected licencse
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * #1830 corrected licence
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * #1830 corrected version
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    * corrected snapshot version
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>
    
    ---------
    
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- kernel-cbeffutil-api/src/test/java/io/mosip/kernel/cbeffutil/test/CbeffImplTest.java
+++ kernel-cbeffutil-api/src/test/java/io/mosip/kernel/cbeffutil/test/CbeffImplTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.mockStatic;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -270,7 +271,8 @@ public class CbeffImplTest {
 					 mockStatic(io.mosip.kernel.core.cbeffutil.common.CbeffXSDValidator.class)) {
 
 			byte[] xmlBytes = "<xml>test</xml>".getBytes();
-			mockedValidator.when(() -> io.mosip.kernel.core.cbeffutil.common.CbeffXSDValidator.validateXML(any(), any()))
+			mockedValidator.when(() -> io.mosip.kernel.core.cbeffutil.common.CbeffXSDValidator.validateXML(
+							nullable(byte[].class), any(byte[].class)))
 					.thenReturn(true);
 
 			boolean result = cbeffUtilImpl.validateXML(xmlBytes);
